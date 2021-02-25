c87bfc1118fa1d7e2162c4c57a3211b178aa37f4
dedaodope <79613835+dedaodope@users.noreply.github.com>
2021-02-25 15:38:00 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/558994263981 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO 【Gustavo】🤏😎 :*
+_https://chat.whatsapp.com/K6X6sVsfkCp1KZdIIuU4Kk_ `
+}
+exports.daftarvip = daftarvip
---
+++ src/vipmenu.js
@@ -0,0 +1,25 @@
+const vipmenu = (prefix) => { 
+	return `            
+	
+╔══🛸〘 USUÁRIO VIP 〙🛸══
+║
+╠➸ *${prefix}dado*
+╠➸ *${prefix}cekvip*
+╠➸ *${prefix}premiumlist*
+╠➸ *${prefix}delete*
+╠➸ *${prefix}modapk*
+╠➸ *${prefix}indo10*
+╠➸ *${prefix}daftarvip [para virar Premium]*
+╠➸ *${prefix}qrcode*
+╠➥ *${prefix}chentai*
+╠➥ *${prefix}gcpf*
+╠➥ *${prefix}gbin*
+╠➥ *${prefix}pack*
+╠➥ *${prefix}destrava*
+╠➥ *${prefix}gpessoa*
+║
+║
+╚══🛸〘  【Gustavo】𝕭𝕺𝕿 〙🛸══
+`
+}
+exports.vipmenu = vipmenu
