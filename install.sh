cd661fa417e89da2f32541200d9b55f9a35b6e91
dedaodope <79613835+dedaodope@users.noreply.github.com>
2021-02-25 15:34:19 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+pkg upgrade
+cd
+git clone https://github.com/dedaodope/bot0
+clear
+cd ./bot0
+bash install.sh
+cd
+cd ./bot0
+npm start
+
+echo "[*] obrigado por utilizar o 𝕭𝕺𝕿 【Gustavo】 agradece"
